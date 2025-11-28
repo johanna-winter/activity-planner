58466d6d4ae13fb73fd0495510ba5e9de8b97c10
Anne Lüken <anne.lkn@hotmail.com>
2025-11-28 12:09:51 +0100

    also added categories list

---
+++ pages/api/categories/index.js
@@ -0,0 +1,15 @@
+import dbConnect from "@/db/connect";
+
+import Category from "@/db/models/Category";
+
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const categories = await Category.find();
+    response.status(200).json(categories);
+    return;
+  }
+
+  response.status(405).json({ status: "Method not allowed" });
+}
