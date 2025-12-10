7ba27b7995bae39f8c51a24116bc04ad89d34087
Anne Lüken <anne.lkn@hotmail.com>
2025-12-10 14:38:08 +0100

    fixed import

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import { ActivityListProvider } from "@/components/ActivitiesList";
-import ActivityForm from "@/components/ActivityForm";
+import ActivityForm from "@/components/ActivityForm/ActivityForm";
 
 export default function HomePage() {
   return (
